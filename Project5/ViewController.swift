753cda809e7b8fd2c0d1c6be5c21cb9b6882b9a4
Charles Martin Reed <charlesmartinreed@icloud.com>
2018-09-05 17:18:09 -0500

    Initial Commit

---
+++ Project5/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project5
+//
+//  Created by Charles Martin Reed on 9/5/18.
+//  Copyright © 2018 Charles Martin Reed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
